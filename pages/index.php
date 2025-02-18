a4b63cafc24ef30377921b43d7e74b26789802de
Benazeth Léo <leo.benazeth@ynov.com>
2025-02-18 16:03:56 +0100

    p

--- pages/index.php
+++ pages/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once 'includes/db.php';
-require_once 'includes/header.php';
+require_once '../includes/db.php';
+require_once '../includes/header.php';
 
 // Fetch all articles from the database, sorted by publication date (newest first)
 $stmt = $pdo->query("SELECT * FROM articles ORDER BY published_at DESC");
@@ -16,7 +16,7 @@ $articles = $stmt->fetchAll();
     <ul class="product-list">
         <?php foreach ($articles as $article): ?>
             <li class="product-item">
-                <a href="./product_detail.php?id=<?= htmlspecialchars($article['id']) ?>">
+                <a href="/pages/product_detail.php?id=<?= htmlspecialchars($article['id']) ?>">
                     <img src="<?= htmlspecialchars($article['image_url']) ?>" alt="<?= htmlspecialchars($article['name']) ?>" width="150">
                     <h2><?= htmlspecialchars($article['name']) ?></h2>
                     <p><?= htmlspecialchars($article['description']) ?></p>
@@ -27,4 +27,4 @@ $articles = $stmt->fetchAll();
     </ul>
 <?php endif; ?>
 
-<?php require_once 'includes/footer.php'; ?>
+<?php require_once '../includes/footer.php'; ?>
