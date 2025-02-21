45df590c055d96f17ac103268a417ba4202e4abd
Benazeth Léo <leo.benazeth@ynov.com>
2025-02-21 10:05:55 +0100

    save all

--- includes/db.php
+++ includes/db.php
@@ -2,8 +2,8 @@
 // Database connection
 $host = 'localhost';
 $dbname = 'php_exam';
-$username = 'leo';
-$password = 'leo';
+$username = 'xxxx';
+$password = 'xxxx';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
